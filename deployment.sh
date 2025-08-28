6195fd0809eb1a047470a4eea955b602cad21afb
PlayaOW <Rayhanulkarim313@gmail.com>
2025-08-28 00:06:03 -0400

    None

---
+++ deployment.sh
@@ -0,0 +1,11 @@
+# Author: Rayhanul Karim
+# Development Date: 08/27/2025
+# Program Name: deployment.sh
+# Description: Will be added
+
+#!/bin/bash
+
+echo "This Program Uses XAMPP to automate your web application deployment"
+echo "For more information visit: https://web-development-class.readthedocs.io/en/latest/semester_1/06_server_management/02_web_server_setup/web_server_setup.html"
+
+if [[ 
