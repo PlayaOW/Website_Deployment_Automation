c1d27652ae6edc85a6b4edd30f1b526ded03b91f
PlayaOW <Rayhanulkarim313@gmail.com>
2025-09-04 21:21:50 -0400

    Fixed

--- deployment.sh
+++ deployment.sh
@@ -10,7 +10,7 @@ countDown(){
 	echo "Starting countdown of $count seconds for root password entree"
 
 	while [ $count -gt 0 ]; do
-		echo $count
+		echo $count "->" \n
 		sleep 1
 		count=$((count - 1))
 	done
@@ -19,10 +19,10 @@ countDown(){
 echo "This Program Uses XAMPP to automate your web application deployment(Requires root login)"
 echo "For more information visit: https://web-development-class.readthedocs.io/en/latest/semester_1/06_server_management/02_web_server_setup/web_server_setup.html"
 DEFAULT_DOWNLOAD="/home/$(whoami)/Downloads"
-countDown
+#countDown
 #udo su -
 
-if [[ ! -d "/opt/lampps" ]]; then
+if [[ ! -d "/opt/lampp" ]]; then
 	echo "XAMPP is not installed"
  	echo "Would you like to install XAMPP?(Y/n)"
  	read CHOICE
@@ -33,11 +33,14 @@ fi
 
 case $CHOICE in
 	Y|y)
-		sudo curl -L -o "$DEFAULT_DOWNLOAD/xampp-linux-x64-8.2.12-0-installer.run" \
-		-A "Mozilla/5.0" \
-		https://www.apachefriends.org/xampp-files/8.2.12/xampp-linux-x64-8.2.12-0-installer.run
-		sudo chmod +x "$DEFAULT_DOWNLOAD/xampp-linux-x64-8.2.12-0-installer.run"
-		sudo bash "$DEFAULT_DOWNLOAD/xampp-linux-x64-8.2.12-0-installer.run"
+		cd $DEFAULT_DOWNLOAD
+		wget "https://downloads.sourceforge.net/project/xampp/XAMPP%20Linux/8.0.30/xampp-linux-x64-8.0.30-0-installer.run"
+		#echo "Downloading XAMPP to $DEFAULT_DOWNLOAD"
+		#echo "Please wait for the download to complete, script will continue in 240 seconds"
+		#sleep 240
+		echo "Installing XAMPP"
+		chmod +x xampp-linux-x64-8.0.30-0-installer.run
+		sudo ./xampp-linux-x64-8.0.30-0-installer.run
 		;;
 	N|n)
 		exit
